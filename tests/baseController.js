466862b3c0bebb6c7cf3e168f09a362908e7e2ab
Esen Sagynov <kadishmal@gmail.com>
2012-12-19 00:11:41 +0900

    Added a test suit for a base controller.

---
+++ tests/baseController.js
@@ -0,0 +1,23 @@
+var should = require("should"),
+    requirejs = require('requirejs');
+
+requirejs.config({
+    baseUrl: 'pulsr',
+    nodeRequire: require
+});
+
+describe('Pulsr', function (){
+    describe('base', function (){
+        it('should return 200 statusCode for root page request within 15s', function (done){
+            // set timeout to 15s
+            this.timeout(15000);
+
+            requirejs(['http', 'conf'], function (http, conf) {
+                http.get('http://' + conf.app.domains.root, function (response) {
+                    response.should.have.status(200);
+                    done();
+                });
+            });
+        });
+    });
+});

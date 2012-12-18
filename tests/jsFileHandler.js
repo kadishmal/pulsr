5ba356e168c6b9004d31e994e81a233ef41ef8f5
Esen Sagynov <kadishmal@gmail.com>
2012-12-18 22:05:43 +0900

    Test: requesting a non-existing JS file should return 404 status code.

--- tests/jsFileHandler.js
+++ tests/jsFileHandler.js
@@ -61,57 +61,25 @@ describe('Pulsr', function (){
                     });
                 });
             });
-        });
 
-        describe('less.js', function (){
-            it('should return 200 statusCode for all LESS file requests within 20s', function (done){
+            it('should return 404 statusCode for non-existing JS file requests within 20s', function (done){
                 // set timeout to 20s
                 this.timeout(20000);
 
                 requirejs(['async', 'fs', 'path', 'module', 'http', 'conf'], function (async, fs, path, module, http, conf) {
-                    // a list of directories to search for LESS files
-                    var jsDirs = ['less'];
+                    // a list of non-existing JS files
+                    var jsFiles = ['js/apple.js', 'js/banana.js', 'js/libs/bootstrap/jake.js', 'zoo.js'];
 
-                    function traverseDir(dirName, done) {
-                        var dirPath = path.join(path.dirname(path.resolve(module.uri)), '../', dirName);
+                    function requestFile(fileName, done) {
+                        var filePath = path.join(path.dirname(path.resolve(module.uri)), '../', fileName);
 
-                        function requestFile(fileName, done) {
-                            if (/.+\.less/.test(fileName)) {
-                                var filePath = path.join(dirPath, fileName);
-
-                                fs.stat(filePath, function (err, stat) {
-                                    if (err) {
-                                        throw err;
-                                    }
-                                    else if (stat.isFile()){
-                                        http.get('http://' + conf.app.domains.static + '/' + dirName + '/' + fileName, function (response) {
-                                            response.should.have.status(200);
-                                            done();
-                                        });
-                                    }
-                                    else{
-                                        done();
-                                    }
-                                });
-                            }
-                            else{
-                                done();
-                            }
-                        }
-
-                        fs.readdir(dirPath, function (err, files) {
-                            if (err) {
-                                throw err;
-                            }
-                            else{
-                                async.forEach(files, requestFile, function (err) {
-                                    done();
-                                });
-                            }
+                        var request = http.get('http://' + conf.app.domains.static + '/' + fileName, function (response) {
+                            response.should.have.status(404);
+                            done();
                         });
                     }
 
-                    async.forEach(jsDirs, traverseDir, function (err) {
+                    async.forEach(jsFiles, requestFile, function (err) {
                         done();
                     });
                 });

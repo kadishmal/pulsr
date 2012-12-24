638785edde19849750a59c365ba6034b983278a9
Esen Sagynov <kadishmal@gmail.com>
2012-12-24 13:27:58 +0900

    Test: for RestController.

---
+++ tests/restController.js
@@ -0,0 +1,39 @@
+var should = require("should"),
+    requirejs = require('requirejs');
+
+requirejs.config({
+    baseUrl: 'pulsr',
+    nodeRequire: require
+});
+
+describe('Pulsr', function (){
+    describe('restController', function (){
+        it('should return 200 statusCode and a JSON object as a body for /api', function (done){
+            // set timeout to 15s
+            this.timeout(15000);
+
+            requirejs(['http', 'conf'], function (http, conf) {
+                http.get('http://' + conf.app.domains.root + '/api', function (response) {
+                    response.should.have.status(200);
+
+                    var body = '';
+
+                    response.on('data', function (chunk) {
+                        body += chunk;
+                    });
+
+                    response.on('end', function () {
+                        body.should.equal(JSON.stringify({
+                            controller: 'api',
+                            method: 'GET',
+                            action: 'default',
+                            status: 'success'
+                        }));
+
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});

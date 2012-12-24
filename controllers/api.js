ee0f6692e9106faf17da85757500db1d8619f4b0
Esen Sagynov <kadishmal@gmail.com>
2012-12-24 13:34:15 +0900

    Added docco compatible documentation comments.

--- controllers/api.js
+++ controllers/api.js
@@ -1,20 +1,26 @@
-/*
-    # api.js
-
-    An exemplary headless controller module to handle client site API actions.
-
-    **Usage:**
-
-    - ** /api**: will run the **default** action as no action is specified.
-    - ** /api/someGetAction**: will run **someGetAction**.
- */
+//
+// An exemplary headless controller to handle client site API actions. Users can either extend this module by adding more functionality to it, or create their own based on this code.
+//
+// **Usage**
+//
+// 1. Must override properties  (at least `moduleId`) of a **RestController** (*/pulsr/restController.js*).
+// 2. Optionally can provide `GET`, `POST`, `DELETE`, etc. properties like `get-actions` shown above.
+//     - When a `GET` request for */api/someGetAction* is arrived, the **RestController** will check if `get-actions` property exists. If it exists, it checks whether or not `someGetAction` is defined in `get-actions`. If it does, it calls the function assigned to that action to handle the request.
+//     - In case no action is requested, eg. */api* with no action specified, the **default** action will be executed. If no default action is provided, 405 Method Not Allowed response is returned.
+//
+// **Requesting**
+//
+// - **/api**: will run the **default** action as no action is specified.
+// - **/api/someGetAction**: will run **someGetAction**.
+//
 define(['restController', 'module', 'conf'], function(RestController, module, conf) {
     return RestController.override({
         moduleId: module.id,
+        // This controller will respond to `GET` requests for those actions only which are indicated in this hash.
         'get-actions': {
-            // a default action if no action is specified
+            // A default action if no action is specified.
             default: function (request, response) {
-                // as a sample response return this API and action info
+                // As a sample response return this API and action info.
                 response.end(JSON.stringify({
                     controller: 'api',
                     method: 'GET',
@@ -22,11 +28,11 @@ define(['restController', 'module', 'conf'], function(RestController, module, co
                     status: 'success'
                 }));
             },
-            // some other API called from a client
+            // Some other API called from a client.
             someGetAction: function (request, response) {
-                // for example, first make sure this request is generated from our own site.
+                // For example, first make sure this request is generated from our own site.
                 if (request.headers && request.headers['host'] && request.headers['host'].indexOf(conf.app.domains.root) == 0) {
-                    // as a sample response return this API and action info
+                    // As a sample response return this API and action info.
                     response.end(JSON.stringify({
                         controller: 'api',
                         method: 'GET',
@@ -35,13 +41,13 @@ define(['restController', 'module', 'conf'], function(RestController, module, co
                     }));
                 }
                 else{
-                    // otherwise, it's prohibited
+                    // Otherwise, it's prohibited
                     response.statusCode = 403;
                     response.end();
                 }
             }
-            // other GET actions can be added here
+            // Other GET actions can be added here.
         }
-        // other actions can be added here
+        // Other HTTP actions can be added here.
     });
 });

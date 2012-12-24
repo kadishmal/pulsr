8600d2a97d462e6164c0d57b18ce98899dc4d362
Esen Sagynov <kadishmal@gmail.com>
2012-12-24 19:03:58 +0900

    Test: additional test for RestController.

--- tests/restController.js
+++ tests/restController.js
@@ -35,5 +35,33 @@ describe('Pulsr', function (){
                 });
             });
         });
+
+        it('should return 200 statusCode and a JSON object as a body for /api/someGetAction', function (done){
+            // set timeout to 15s
+            this.timeout(15000);
+
+            requirejs(['http', 'conf'], function (http, conf) {
+                http.get('http://' + conf.app.domains.root + '/api/someGetAction', function (response) {
+                    response.should.have.status(200);
+
+                    var body = '';
+
+                    response.on('data', function (chunk) {
+                        body += chunk;
+                    });
+
+                    response.on('end', function () {
+                        body.should.equal(JSON.stringify({
+                            controller: 'api',
+                            method: 'GET',
+                            action: 'someGetAction',
+                            status: 'success'
+                        }));
+
+                        done();
+                    });
+                });
+            });
+        });
     });
 });

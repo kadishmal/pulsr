e0ae1d49c3aa5e7520d8cd13ec8bff2ab246d587
Esen Sagynov <kadishmal@gmail.com>
2013-01-05 23:55:32 +0900

    Test: added test cases to fileHandler to request favicon icons.

--- tests/fileHandler.js
+++ tests/fileHandler.js
@@ -85,7 +85,7 @@ describe('Pulsr', function (){
             this.timeout(20000);
 
             requirejs(['async', 'http', 'conf'], function (async, http, conf) {
-                var restrictedDirs = ['img', 'js', 'less', 'pulsr', 'controller', 'nonExistingDir', 'js/bootstrap', 'less/foundation'];
+                var restrictedDirs = ['img', 'js', 'less', 'pulsr', 'controller', 'nonExistingDir', 'js/bootstrap', 'less/foundation', 'assets'];
 
                 function requestDir(dirName, done) {
                     http.get('http://' + conf.app.domains.static + '/' + dirName, function (response) {
@@ -105,7 +105,7 @@ describe('Pulsr', function (){
             this.timeout(20000);
 
             requirejs(['async', 'http', 'conf'], function (async, http, conf) {
-                var restrictedDirs = ['pulsr/conf.js', 'pagelets/ga/ga.js', 'contents/404.md', 'controllers/api.js', 'node_modules/bin/docco', 'nonExistingDir', 'tests/baseController.js', 'views/layout.hb', '../aboveRootNonExistDir'];
+                var restrictedDirs = ['pulsr/conf.js', 'pagelets/ga/ga.js', 'contents/404.md', 'controllers/api.js', 'node_modules/bin/docco', 'nonExistingDir', 'tests/baseController.js', 'views/layout.hb', '../aboveRootNonExistDir', 'app.js', '.travis.yml', '.gitignore', 'Makefile', 'package.json', 'assets'];
 
                 function requestDir(dirName, done) {
                     http.get('http://' + conf.app.domains.static + '/' + dirName, function (response) {
@@ -115,6 +115,49 @@ describe('Pulsr', function (){
                 }
 
                 async.forEach(restrictedDirs, requestDir, function (err) {
+                    done();
+                });
+            });
+        });
+
+
+        it('should return 200 statusCode for favicon requests located in the root directory.', function (done){
+            // set timeout to 20s
+            this.timeout(20000);
+
+            requirejs(['async', 'http', 'conf'], function (async, http, conf) {
+                var favicons = ['favicon.ico', 'apple-touch-icon-57x57-precomposed.png'];
+
+                function sendRequest(filePath, done) {
+                    http.get('http://' + conf.app.domains.static + '/' + filePath, function (response) {
+                        response.should.have.status(200);
+                        done();
+                    });
+                }
+
+                async.forEach(favicons, sendRequest, function (err) {
+                    console.log('favicon is done');
+                    done();
+                });
+            });
+        });
+
+        it('should return 200 statusCode for plain text requests located in the root directory.', function (done){
+            // set timeout to 20s
+            this.timeout(20000);
+
+            requirejs(['async', 'http', 'conf'], function (async, http, conf) {
+                var favicons = ['robots.txt', 'humans.txt', 'README.md', 'CHANGELOG.md'];
+
+                function sendRequest(filePath, done) {
+                    http.get('http://' + conf.app.domains.static + '/' + filePath, function (response) {
+                        response.should.have.status(200);
+                        done();
+                    });
+                }
+
+                async.forEach(favicons, sendRequest, function (err) {
+                    console.log('favicon is done');
                     done();
                 });
             });

93cb55743ebf0ed4acebee76e124fdc380af2797
Esen Sagynov <kadishmal@gmail.com>
2013-02-10 22:30:01 +0900

    Enh: redirect root domain requests to www subdomain.

--- pulsr/url_manager.js
+++ pulsr/url_manager.js
@@ -6,63 +6,75 @@ define(['url_patterns', 'requirejs', 'conf', 'path', 'session', 'error_handler',
     }
 
     return function(request, response) {
-        var match, pulsrController;
-
-        function callback(controller) {
-            // start session here for all user defined controllers
-            // pulsr fileManager controller should not set any
-            // session information for static resources.
-            // All cookies for static resources should be ignored.
-            session.start(request, response);
-            controller.handle(request, response);
+        // If the hostname misses the "www" part, redirect to "www" domain.
+        // The allows to use cookie-free domains for static files.
+        // Refer to http://developer.yahoo.com/performance/rules.html#cookie_free
+        if ('www.' + request.headers.host == conf.app.domains.root) {
+            // 301: Permanently Moved.
+            response.writeHead(301, {
+                'Location': 'http://www.' + request.headers.host + request.url
+            });
+            response.end();
         }
+        else{
+            var match, pulsrController;
 
-        function errback(err) {
-            // this function is called when no user defined controller
-            // is found. In such case it falls back and searches among
-            // Pulsr's controller.
-            // No Pulsr controller is called here except for fileHandler
-            // which ignores cookies and should not set any session data.
-            requirejs([pulsrController], function (controller) {
+            function callback(controller) {
+                // start session here for all user defined controllers
+                // pulsr fileManager controller should not set any
+                // session information for static resources.
+                // All cookies for static resources should be ignored.
+                session.start(request, response);
                 controller.handle(request, response);
-            }, function (err) {
-                console.log(err);
-                // Internal Server Error. This should not have happened
-                // as fileHandler controller MUST BE in Pulsr.
-                error_handler.handle(request, response, {errno: 500});
+            }
+
+            function errback(err) {
+                // this function is called when no user defined controller
+                // is found. In such case it falls back and searches among
+                // Pulsr's controller.
+                // No Pulsr controller is called here except for fileHandler
+                // which ignores cookies and should not set any session data.
+                requirejs([pulsrController], function (controller) {
+                    controller.handle(request, response);
+                }, function (err) {
+                    console.log(err);
+                    // Internal Server Error. This should not have happened
+                    // as fileHandler controller MUST BE in Pulsr.
+                    error_handler.handle(request, response, {errno: 500});
+                });
+            }
+
+            request.body = '';
+            // retrieve the request data in one tick.
+            request.on('data', function (chunk) {
+                request.body += chunk;
+
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                // Also checking the domain would be good
+                if (request.body.length > 1e6) {
+                    request.connection.destroy();
+                    // TODO: we may temporarily add to a blacklist
+                }
+            });
+            // once all data has been received from the client,
+            // handle the request.
+            request.on('end', function () {
+                for (var pattern in regExpStore) {
+                    if ((match = regExpStore[pattern].exec(request.url))) {
+                        var controllerPath = path.join(conf.dir.controllers, url_patterns[pattern]);
+
+                        pulsrController = url_patterns[pattern];
+                        request.params = match;
+
+                        // JSlint doesn't like when functions are defined inside for loops,
+                        // so I'm taking these functions out.
+                        requirejs([controllerPath], callback, errback);
+
+                        // break the loop since we've already found the first match
+                        return false;
+                    }
+                }
             });
         }
-
-        request.body = '';
-        // retrieve the request data in one tick.
-        request.on('data', function (chunk) {
-            request.body += chunk;
-
-            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-            // Also checking the domain would be good
-            if (request.body.length > 1e6) {
-                request.connection.destroy();
-                // TODO: we may temporarily add to a blacklist
-            }
-        });
-        // once all data has been received from the client,
-        // handle the request.
-        request.on('end', function () {
-            for (var pattern in regExpStore) {
-                if ((match = regExpStore[pattern].exec(request.url))) {
-                    var controllerPath = path.join(conf.dir.controllers, url_patterns[pattern]);
-
-                    pulsrController = url_patterns[pattern];
-                    request.params = match;
-
-                    // JSlint doesn't like when functions are defined inside for loops,
-                    // so I'm taking these functions out.
-                    requirejs([controllerPath], callback, errback);
-
-                    // break the loop since we've already found the first match
-                    return false;
-                }
-            }
-        });
 	};
 });

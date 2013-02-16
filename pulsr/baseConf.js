kadishmal/pulsr

commit 62bb24db087970a9995a55cf1a797243db243dc1

Enh: allow to custom set maxAge for layouts and stats cache.

pulsr/baseConf.js
@@ -15,18 +15,30 @@ define(['module', 'path', 'microConf'], function(module, path, conf) {
                     300),
                 fileCache: {
                     stats: {
-                        // options passed directly to the internal lru cache
-                        // indicating how many items to store in a fileCache
+                        // Options passed directly to the internal lru cache
+                        // indicating how many items to store in a fileCache.
                         max: 100,
-                        // refresh the cache every 10 minutes
-                        maxAge: 1000 * 60 * 10
+                        // maxAge property for lru cache.
+                        maxAge: function () {
+                            return (isProduction ? conf.get('app.cache.fileCache.stats.maxAgeProduction') : conf.get('app.cache.fileCache.stats.maxAgeDev')) * 60 * 1000;
+                        },
+                        // Don't cache file stats in dev environment.
+                        maxAgeDev: 0,
+                        // In production cache file stats for 10 minutes.
+                        maxAgeProduction: 10
                     },
                     layouts: {
-                        // options passed directly to the internal lru cache
-                        // indicating how many items to store in a fileCache
+                        // Options passed directly to the internal lru cache
+                        // indicating how many items to store in a fileCache.
                         max: 100,
-                        // refresh the cache every 10 minutes
-                        maxAge: 1000 * 60 * 10
+                        // maxAge property for lru cache.
+                        maxAge: function () {
+                            return (isProduction ? conf.get('app.cache.fileCache.stats.maxAgeProduction') : conf.get('app.cache.fileCache.stats.maxAgeDev')) * 60 * 1000;
+                        },
+                        // Don't cache file stats in dev environment.
+                        maxAgeDev: 0,
+                        // In production cache file stats for 10 minutes.
+                        maxAgeProduction: 10
                     }
                 }
             },

commit fa8283a62893f8c7801e60248aa0f82e20912295

Fix: a copy paste bug introduced in 62bb24db087970a9995a55cf1a797243db243dc1.

pulsr/baseConf.js
@@ -33,7 +33,7 @@ define(['module', 'path', 'microConf'], function(module, path, conf) {
                         max: 100,
                         // maxAge property for lru cache.
                         maxAge: function () {
-                            return (isProduction ? conf.get('app.cache.fileCache.stats.maxAgeProduction') : conf.get('app.cache.fileCache.stats.maxAgeDev')) * 60 * 1000;
+                            return (isProduction ? conf.get('app.cache.fileCache.layouts.maxAgeProduction') : conf.get('app.cache.fileCache.layouts.maxAgeDev')) * 60 * 1000;
                         },
                         // Don't cache file stats in dev environment.
                         maxAgeDev: 0,